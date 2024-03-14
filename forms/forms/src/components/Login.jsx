72ea1e2cd3b33ca4e1fa62ce6059df4e37925b8e
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-14 23:18:18 +0530

    user-input-handler-using-state

--- forms/forms/src/components/Login.jsx
+++ forms/forms/src/components/Login.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function Login() {
-  function handleSubmit() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  function handleSubmit(event) {
     event.preventDefault();
     console.log("submitted");
   }
+
+  function handleEmailChange(event) {
+    setUserEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setUserPassword(event.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -10,12 +23,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={userEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={userPassword}
+          />
         </div>
       </div>
 
@@ -25,9 +50,7 @@ export default function Login() {
         {/* <button type="button" className="button" onClick={handleSubmit}>
           Login
         </button> */}
-        <button className="button" >
-          Login
-        </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
