acadea999aacfe7f931a01f23f5188829fcdaf46
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-16 00:35:16 +0530

    validating-input-via-inbuilts-(html-form-validation)

--- forms/forms/src/components/Signup.jsx
+++ forms/forms/src/components/Signup.jsx
@@ -17,13 +17,13 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" required/>
         </div>
   
         <div className="control-row">
           <div className="control">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input id="password" type="password" name="password" required/>
           </div>
   
           <div className="control">
@@ -32,6 +32,8 @@ export default function Signup() {
               id="confirm-password"
               type="password"
               name="confirm-password"
+              required
+              minLength={6}
             />
           </div>
         </div>
@@ -91,7 +93,7 @@ export default function Signup() {
   
         <div className="control">
           <label htmlFor="terms-and-conditions">
-            <input type="checkbox" id="terms-and-conditions" name="terms" />I
+            <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
             agree to the terms and conditions
           </label>
         </div>
