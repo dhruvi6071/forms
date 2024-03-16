40f855686dc3957737e2bb033582b509c49540ad
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-16 19:55:07 +0530

    mixing-builtIn-and-custom-logic

--- forms/forms/src/components/Signup.jsx
+++ forms/forms/src/components/Signup.jsx
@@ -1,112 +1,127 @@
+import { useState } from "react";
+
 export default function Signup() {
-  function handleSubmit(event){
+
+  function handleSubmit(event) {
+    const [passwordAreNotEqual, setPasswordAreNotEqual] = useState(false);
     event.preventDefault();
 
     const fd = new FormData(event.target); //To get access for the all values that are going to be inserted, every input and select fields must have "name" property.
-    const acquisitionChannel = fd.getAll('');
-    const data =Object.fromEntries(fd.entries());
+    const acquisitionChannel = fd.getAll("");
+    const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if(data.password !== data['confirm-password']){
+      setPasswordAreNotEqual(true);
+      return;
+    }
     console.log(data);
 
     // event.target.reset();    //It is another method and does the same as type of the button is reset.
   }
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Welcome on board!</h2>
-        <p>We just need a little bit of data from you to get you started 🚀</p>
-  
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" required />
+      </div>
+
+      <div className="control-row">
         <div className="control">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required/>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" required />
         </div>
-  
-        <div className="control-row">
-          <div className="control">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" required/>
-          </div>
-  
-          <div className="control">
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              id="confirm-password"
-              type="password"
-              name="confirm-password"
-              required
-              minLength={6}
-            />
-          </div>
-        </div>
-  
-        <hr />
-  
-        <div className="control-row">
-          <div className="control">
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" name="first-name" />
-          </div>
-  
-          <div className="control">
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" name="last-name" />
-          </div>
-        </div>
-  
+
         <div className="control">
-          <label htmlFor="phone">What best describes your role?</label>
-          <select id="role" name="role">
-            <option value="student">Student</option>
-            <option value="teacher">Teacher</option>
-            <option value="employee">Employee</option>
-            <option value="founder">Founder</option>
-            <option value="other">Other</option>
-          </select>
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            required
+            minLength={6}
+          />
+          <div className="control-error">{passwordAreNotEqual && <p>Passwords should match</p>}</div>
         </div>
-  
-        <fieldset>
-          <legend>How did you find us?</legend>
-          <div className="control">
-            <input
-              type="checkbox"
-              id="google"
-              name="acquisition"
-              value="google"
-            />
-            <label htmlFor="google">Google</label>
-          </div>
-  
-          <div className="control">
-            <input
-              type="checkbox"
-              id="friend"
-              name="acquisition"
-              value="friend"
-            />
-            <label htmlFor="friend">Referred by friend</label>
-          </div>
-  
-          <div className="control">
-            <input type="checkbox" id="other" name="acquisition" value="other" />
-            <label htmlFor="other">Other</label>
-          </div>
-        </fieldset>
-  
+      </div>
+
+      <hr />
+
+      <div className="control-row">
         <div className="control">
-          <label htmlFor="terms-and-conditions">
-            <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
-            agree to the terms and conditions
-          </label>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" />
         </div>
-  
-        <p className="form-actions">
-          {/* Here type reset is inbuilt functionality of jacaqScript to reset values */}
-          <button type="reset" className="button button-flat">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Sign up
-          </button>
-        </p>
-      </form>
-    );
-  }
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role">
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="form-actions">
+        {/* Here type reset is inbuilt functionality of jacaqScript to reset values */}
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
+}
