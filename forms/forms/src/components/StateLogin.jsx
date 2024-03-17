3d0796cd8362030b6a6f91485980aafddf29877c
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-17 22:29:23 +0530

    make-code-more-linear

--- forms/forms/src/components/StateLogin.jsx
+++ forms/forms/src/components/StateLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Input from "./Input";
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -10,10 +10,11 @@ export default function Login() {
 
   const [didEdit, setDidEdit] = useState({
     email: false,
-    password: false
+    password: false,
   });
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const passwordIsInvalid = didEdit.password && enteredValues.password.trim().length<6;
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -30,8 +31,8 @@ export default function Login() {
 
   function handleInputBlur(identifier) {
     setDidEdit((prevEdit) => ({
-        ...prevEdit,
-        [identifier]: false
+      ...prevEdit,
+      [identifier]: false,
     }));
   }
   // function handleEmailChange(event) {
@@ -47,22 +48,45 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={() => handleInputBlur("email")} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
+          onChange={(event) => handleInputChange("email", event.target.value)}
+          value={enteredValues.email}
+          error={emailIsInvalid && "Please enter a valid Email"}
+        />
+
+
+        {/* <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input
             id="email"
             type="email"
             name="email"
-            onBlur={() => handleInputBlur('email')}  //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
+            onBlur={() => handleInputBlur("email")} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
           <div className="control-error">
             {emailIsInvalid && <p>Please enter valid email id</p>}
           </div>
-        </div>
+        </div> */}
 
-        <div className="control no-margin">
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={() => handleInputBlur("password")} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
+          onChange={(event) => handleInputChange("password", event.target.value)}
+          value={enteredValues.password}
+          error = {passwordIsInvalid && "Please enter a valid Password"}
+        />
+
+        {/* <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input
             id="password"
@@ -73,8 +97,9 @@ export default function Login() {
             }
             value={enteredValues.password}
           />
-        </div>
-      </div>
+          </div> */}
+
+      </div> 
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
