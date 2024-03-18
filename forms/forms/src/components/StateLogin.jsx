0c69043c3daa257b64556e958623e8b410378f2d
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-18 12:20:15 +0530

    useInput-custom-hook

--- forms/forms/src/components/StateLogin.jsx
+++ forms/forms/src/components/StateLogin.jsx
@@ -1,49 +1,41 @@
-import { useState } from "react";
 import Input from "./Input.jsx";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
+
 export default function Login() {
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordhasError
+  } = useInput('', (value) => hasMinLength(value, 6));
+
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [enteredPassword, setEnteredPassword] = useState('');
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid =
-    didEdit.email &&
-    !isEmail(enteredValues.email) &&
-    !isNotEmpty(enteredValues.email);
+  // const emailIsInvalid =
+  //   didEdit.email &&
+  //   !isEmail(enteredValues.email) &&
+  //   !isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && hasMinLength(enteredValues.password, 6);
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(enteredValues);
+    if(emailHasError || passwordhasError){
+      return ;
+    }
+    console.log(emailValue, passwordValue);
     // email.current.value = "";
   }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit((prevEdit) => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
   // function handleEmailChange(event) {
   //   setEnteredEmail(event.target.value);
   // }
@@ -62,10 +54,10 @@ export default function Login() {
           id="email"
           type="email"
           name="email"
-          onBlur={() => handleInputBlur("email")} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
-          onChange={(event) => handleInputChange("email", event.target.value)}
-          value={enteredValues.email}
-          error={emailIsInvalid && "Please enter a valid Email"}
+          onBlur={handleEmailBlur} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid Email"}
         />
 
         {/* <div className="control no-margin">
@@ -88,12 +80,10 @@ export default function Login() {
           id="password"
           type="password"
           name="password"
-          onBlur={() => handleInputBlur("password")} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
-          onChange={(event) =>
-            handleInputChange("password", event.target.value)
-          }
-          value={enteredValues.password}
-          error={passwordIsInvalid && "Please enter a valid Password"}
+          onBlur={handlePasswordBlur} //the blur event is a built-in default browser event that will be fired whenever this input loses focus.
+          onChange={handlePasswordChange }
+          value={password}
+          error={passwordhasError && "Please enter a valid Password"}
         />
 
         {/* <div className="control no-margin">
---
+++ forms/forms/src/hooks/useInput.js
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+export function useInput(defaultValue, validationFunction) {
+  const [enteredValue, setEnteredValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+const valueIsValid = validationFunction(enteredValue);
+  function handleInputChange(event) {
+    setEnteredValues(event.target.value);
+    setDidEdit(false);
+  }
+
+  function handleInputBlur() {
+    setDidEdit(true);
+  }
+
+  return {
+    value: enteredValue,
+    handleInputChange,
+    handleInputBlur,
+    hasError: didEdit && !valueIsValid
+  };
+}
